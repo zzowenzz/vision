8b16888f0caf450bf397e60b66f230e34ab360a4
Ubuntu <ubuntu@ip-10-0-4-190.ap-southeast-2.compute.internal>
2023-01-04 12:52:57 +0000

    modify network architecture to train FashionMNIST

--- Lenet/model.py
+++ Lenet/model.py
@@ -6,11 +6,11 @@ import torchinfo
 class Lenet(nn.Module):
     def __init__(self):
         super(Lenet, self).__init__()
-        self.conv1 = nn.Conv2d(1, 16, 5)
+        self.conv1 = nn.Conv2d(1, 6, kernel_size=5, padding=2)
         self.pool1 = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(16, 32, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
         self.pool2 = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(32*5*5, 120)
+        self.fc1 = nn.Linear(16*5*5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
     
@@ -19,15 +19,15 @@ class Lenet(nn.Module):
         x = self.pool1(x)            
         x = F.relu(self.conv2(x))    
         x = self.pool2(x)            
-        x = x.view(-1, 32*5*5)       
+        x = x.view(-1, 16*5*5)       
         x = F.relu(self.fc1(x))      
         x = F.relu(self.fc2(x))     
         x = self.fc3(x)              
         return x
 
 # test for shape
-lenet = Lenet()
-report = torchinfo.summary(lenet, input_size=(1,1,32,32))
-summary_report = str(report)
-with open("architecture.txt", "w") as f:
-    f.write((summary_report))
+# lenet = Lenet()
+# report = torchinfo.summary(lenet, input_size=(1,1,32,32))
+# summary_report = str(report)
+# with open("architecture.txt", "w") as f:
+#     f.write((summary_report))
