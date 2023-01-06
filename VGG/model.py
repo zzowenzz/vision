3de0587c3f63665d7e241152c2fdf98332dad16e
Ubuntu <ubuntu@ip-10-0-4-190.ap-southeast-2.compute.internal>
2023-01-06 01:45:36 +0000

    add VGG

---
+++ VGG/model.py
@@ -0,0 +1,74 @@
+import torch
+import torch.nn.functional as F
+import torch.nn as nn 
+import torchinfo
+
+class VGG(nn.Module):
+    def __init__(self, features, num_classes=10):
+        super(VGG, self).__init__()
+        self.features = features
+        self.classifier = nn.Sequential(
+            nn.Linear(512*7*7, 4096),
+            nn.ReLU(True),
+            nn.Dropout(p=0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU(True),
+            nn.Dropout(p=0.5),
+            nn.Linear(4096, num_classes)
+        )
+    def forward(self, x):
+        x = self.features(x)
+        x = torch.flatten(x, start_dim=1)
+        x = self.classifier(x)
+        return x
+
+def make_features(cfg: list):
+    layers = []
+    in_channels = 1
+    for v in cfg:
+        if v == "M":
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        else:
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+            layers += [conv2d, nn.ReLU(True)]
+            in_channels = v
+    return nn.Sequential(*layers)
+
+cfgs = {
+    'vgg11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'vgg13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'vgg16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'vgg19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
+
+def vgg11(model_name="vgg11", **kwargs):
+    assert model_name in cfgs, "Warning: model number {} not in cfgs dict!".format(model_name)
+    cfg = cfgs[model_name]
+
+    model = VGG(make_features(cfg), **kwargs)
+    return model
+def vgg13(model_name="vgg13", **kwargs):
+    assert model_name in cfgs, "Warning: model number {} not in cfgs dict!".format(model_name)
+    cfg = cfgs[model_name]
+
+    model = VGG(make_features(cfg), **kwargs)
+    return model
+def vgg16(model_name="vgg16", **kwargs):
+    assert model_name in cfgs, "Warning: model number {} not in cfgs dict!".format(model_name)
+    cfg = cfgs[model_name]
+
+    model = VGG(make_features(cfg), **kwargs)
+    return model
+def vgg19(model_name="vgg19", **kwargs):
+    assert model_name in cfgs, "Warning: model number {} not in cfgs dict!".format(model_name)
+    cfg = cfgs[model_name]
+
+    model = VGG(make_features(cfg), **kwargs)
+    return model
+    
+# test for shape
+# net = vgg19()
+# report = torchinfo.summary(net, input_size=(1,1,224,224))
+# summary_report = str(report)
+# with open("vgg19.txt", "w") as f:
+#     f.write((summary_report))
