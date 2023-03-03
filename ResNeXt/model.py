520914a4b8c747ccd7ce4d0005a13d867f73080f
Owen Xing <owenxing1994@gmail.com>
2023-03-03 10:56:37 +1000

    update model

--- ResNeXt/model.py
+++ ResNeXt/model.py
@@ -77,10 +77,7 @@ class ResNet(nn.Module):
         if self.include_top:
             self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
             self.fc = nn.Linear(512 * block.expansion, num_classes)
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-
+            
     def _make_layer(self, block, channel, block_num, stride=1):
         downsample = None
         if stride != 1 or self.in_channel != channel * block.expansion:
